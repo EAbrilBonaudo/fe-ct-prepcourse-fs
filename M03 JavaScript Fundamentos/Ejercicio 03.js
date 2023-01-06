9d27d161aaa7637b638e9cfe032ba9156fd11abe
EAbrilBonaudo <abrilbonaudo@gmail.com>
2023-01-06 20:22:27 -0300

    third commit

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -15,7 +15,7 @@ function tienenMismaLongitud(str1, str2) {
    // Retorna True si los dos strings tienen la misma longitud.
    // De lo contrario, retorna False.
    // Tu código:
-   if (str1 === str2) {
+   if (str1.lenGTH == str2.lenGTH) {
       return true;
    }
    return false
@@ -25,24 +25,40 @@ function menosQueNoventa(num) {
    // Retorna True si el argumento "num" es menor que noventa.
    // De lo contrario, retorna False.
    // Tu código:
+   if (num < 90) {
+      return true
+   }
+   return false
 }
 
 function mayorQueCincuenta(num) {
    // Retorna True si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna False.
    // Tu código:
+   if (num > 50) {
+      return true
+   }
+   return false
 }
 
 function esPar(num) {
    // Retorna True si "num" es par.
    // De lo contrario, retorna False.
    // Tu código:
+   if (num % 2 == 0) {
+      return true
+   }
+   else false
 }
 
 function esImpar(num) {
    // Retorna True si "num" es impar.
    // De lo contrario, retorna False.
    // Tu código:
+   if (num % 2 != 0) {
+      return true
+   }
+   return false
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
